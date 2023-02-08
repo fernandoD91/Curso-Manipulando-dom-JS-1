83c531f28ab9c05c7244bab84a8850c2270dbae5
Daniel Fernando Posta <fernando.d91@hotmail.com>
2023-02-08 15:09:22 -0300

    clase 03-js dom

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]")
-    console.log(input.value);
+    const taskName = input.value;
+    input.value = "";
+    const ul = document.querySelector('[data-list]')
+    const li = document.createElement('li');
+    li.classList.add('card')
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task"> ${taskName}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    li.innerHTML = content;
+    ul.appendChild(li);
 }
-
 btn.addEventListener('click', createTask);
